cd9aee8f83f5d3352da95ee08e456ad728911a56
Bear M <bear.mistele@gmail.com>
2024-08-29 19:29:23 -0700

    Route changes for JSX

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import About from './pages/about';
-import Portfolio from './pages/portfolio';
-import Contact from './pages/contact';
-import Navbar from './components/NavBar';
+import About from './pages/about.jsx';
+import Portfolio from './pages/portfolio.jsx';
+import Contact from './pages/contact.jsx';
+import Navbar from './components/NavBar.jsx';
 import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
