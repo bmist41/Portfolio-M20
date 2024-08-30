b5f530c755e639ea9976975e4ecce31f48e7952a
Bear M <bear.mistele@gmail.com>
2024-08-29 20:17:14 -0700

    route fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import About from './pages/about.jsx';
-import Portfolio from './pages/portfolio.jsx';
-import Contact from './pages/contact.jsx';
-import Navbar from './components/NavBar.jsx';
+import About from '../pages/about.jsx';
+import Portfolio from '../pages/portfolio.jsx';
+import Contact from '../pages/contact.jsx';
+import Navbar from '../components/NavBar.jsx';
 import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
